43e9750892908c6635a35d8528426d14d289cc4d
DMau1420 <shootingdarcklight@gmail.com>
2026-02-04 16:33:55 -0600

    Actualizacion de arhivo de instalacion y Readme.md

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-#Instalacion de oh-my-zsh
-sudo pacman -S swww fastfetch blueman bluez bluez-utils eza hyprshot kitty loupe networkmanager network-manager-applet nwg-look neovim pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse rofi sddm sddm-kcm spotify-launcher thunar thunar-archive-plugin tree nerd-fonts ufw waybar yazi
+#Instalacion de Dependencias
+sudo pacman -S swww fastfetch blueman bluez bluez-utils brightnessctl eza hyprshot kitty loupe networkmanager network-manager-applet nwg-look neovim pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse rofi sddm sddm-kcm spotify-launcher thunar thunar-archive-plugin tree nerd-fonts ufw waybar yazi
 
+#instalacion de oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-yay -S eww flat-remix-gtk spicetify-cli sddm-silent-theme 
+#instalacion otras dependencias
+yay -S eww flat-remix-gtk spicetify-cli sddm-silent-theme
+
+
+#instalacion de los dotfiles
+cp config/* ~/.config/
+cp .zshrc ~/
+cp Wallpapers ~/
