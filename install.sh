7bbc7814af97c38cd38c449a99cf24736c0f00c5
Mau <162360856+DMau1420@users.noreply.github.com>
2026-02-04 22:22:25 -0600

    Actualizacion de install.sh
    correccion de archivo de instalacion debido a que no o hacia correctamente

--- install.sh
+++ install.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 #Instalacion de Dependencias
-sudo pacman -S swww fastfetch blueman bluez bluez-utils brightnessctl eza hyprshot kitty loupe networkmanager network-manager-applet nwg-look neovim pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse rofi sddm sddm-kcm spotify-launcher thunar thunar-archive-plugin tree nerd-fonts ufw waybar yazi
 
-#instalacion de oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if ! comand -v yay &> /dev/null; then
+  sudo pacman -S --needed base-devel git -y
+  git clone https://aur.archlinux.org/yay.git -y /tmp/yay
+  cd /tmp/yay && makepkg -si --noconfirm
+  cd
+fi
+
+sudo pacman -S --needed --noconfirm swww fastfetch blueman bluez bluez-utils brightnessctl eza hyprshot kitty loupe networkmanager network-manager-applet nwg-look neovim pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse rofi sddm sddm-kcm spotify-launcher thunar thunar-archive-plugin tree nerd-fonts ufw waybar yazi zsh
 
 #instalacion otras dependencias
-yay -S eww flat-remix-gtk spicetify-cli sddm-silent-theme
+yay -S --noconfirm eww flat-remix-gtk spicetify-cli sddm-silent-theme
 
+#instalacion de oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh"]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+mkdir -p ~/Wallpapers
 
 #instalacion de los dotfiles
-cp config/* ~/.config/
+cp -rv config/* ~/.config/
 cp .zshrc ~/
-cp Wallpapers ~/
+cp -rv Wallpapers/* ~/Wallpapers/
+
+read -p "Quieres Reiniciar ahora? (y/n)" confirm
+if [[ $confirm == [yY] ]]; then
+  sudo reboot
+fi
